e25c99db830b808f8b407a807092723993ae281e
dema96 <fededema96@gmail.com>
2018-07-10 18:34:47 +0200

    inserita libreria jest e modificato funzione area

--- area.js
+++ area.js
@@ -1,12 +1,16 @@
 export.getArea = function(lati){
   if(Array.isArray(lati)){
-    var lato1 = lati[0];
-    var lato2 = lati[1];
-    if(lato1 >= 0 && lato2 >= 0){
-      return lato1*lato2
-    }else{
-      return -1
-    }
+      var lato1 = parseInt(lati[0]);
+      var lato2 = parseInt(lati[1]);
+      if(Number.isInteger(lato1) && Number.isInteger(lato2)){}
+        if(lato1 >= 0 && lato2 >= 0){
+          return lato1*lato2
+        }else{
+          return -1
+        }
+      }else{
+        return -1
+      }
   }else{
     return -1
   }
