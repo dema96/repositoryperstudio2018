9d49e24af76f6c723dd188a4f953a9e140165859
dema96 <fededema96@gmail.com>
2018-07-10 18:54:01 +0200

    fix problemi funzione

--- area.js
+++ area.js
@@ -1,17 +1,13 @@
-export.getArea = function(lati){
+exports.getArea = function(lati){
   if(Array.isArray(lati)){
       var lato1 = parseInt(lati[0]);
       var lato2 = parseInt(lati[1]);
-      if(Number.isInteger(lato1) && Number.isInteger(lato2)){}
-        if(lato1 >= 0 && lato2 >= 0){
-          return lato1*lato2
-        }else{
-          return -1
-        }
+      if(lato1 >= 0 && lato2 >= 0){
+        return lato1*lato2
       }else{
         return -1
       }
   }else{
-    return -1
+    return -3
   }
 }
